82e08313fe5ed084d91df01ab7afb2c72a3ac3b5
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-15 21:21:04 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Víctor Ávila on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
