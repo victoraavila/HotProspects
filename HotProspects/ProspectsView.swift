ba8bcd5764fb0945b37894962c60afd8d5689bcd
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-18 01:56:07 -0300

    Adding options with swipe actions

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -5,17 +5,7 @@
 //  Created by Víctor Ávila on 15/08/24.
 //
 
-// Scanning a QR Code or a Bar Code can be done with Apple's AVFoundation library. Since it does not work very well with SwiftUI, let's download Paul's CodeScanner: File > Add Package Dependencies... > https://github.com/twostraws/CodeScanner. Let Dependency Rule set to Up to Next Major Version and click Add Package.
-// We will use the CodeScannerView, which will be shown inside a sheet and handle code in an isolated way.
-// To start scanning QR Codes:
-// 1. Add an @State property to track whether we are currently showing the scanning view or not.
-
-// The CodeScanner package handles it all: it figures what the code is and how to send it back. All we gotta do is to catch the result and process it somehow. When it finds a code, it will a call a completion closure with a result instance containing details about the code found or an error saying what the problem was (for example, maybe the camera was not available or it could not scan codes).
-
-// Before showing the scanner, we got to ask the user for permission to use their Camera. To do that, go to the HotProspects abstract > Targets > Info > Right click and choose Add Row > Privacy - Camera Usage Description. Add a message to be shown with the prompt by adding it on the Value column.
-
 import CodeScanner
-
 import SwiftData
 import SwiftUI
 
@@ -42,9 +32,11 @@ struct ProspectsView: View {
     
     @State private var isShowingScanner = false
     
+    @State private var selectedProspects = Set<Prospect>() // By default, no selections.
+    
     var body: some View {
         NavigationStack {
-            List(prospects) { prospect in
+            List(prospects, selection: $selectedProspects) { prospect in // The selection is bound to selectedProspects
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
@@ -52,19 +44,55 @@ struct ProspectsView: View {
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions {
+                    // Swipe Actions don't play nicely with the .onDelete() modifier, so we've gotta make Delete work by hand:
+                    // 1. We will add an individual swipe to delete like .onDelete();
+                    // 2. We will also add a multiple selection option that let users remove multiple entries at the same time. This means adding an @State to store the active selection.
+                    // We have to add the Delete Button first in the List so it becomes automatically attached to the full swipe to activate functionality, which is the default behavior.
+                    Button("Delete", systemImage: "trash", role: .destructive) {// The destructive role is because this is a bad Button
+                        modelContext.delete(prospect)
+                        // Remember that the original Swipe to Delete shows the word "Delete" when swipping. However, Apple tells us to use only icons when having multiple options to choose.
+                    }
+                    
+                    // We need a way to move people from the Uncontacted Tab to the Contacted Tab: we will add a Swipe Action to the VStack in ProspectView to do so.
+                    // Since ProspectsView is shared between 3 Tabs, we've got to make sure the Swipe Action is correct no matter where they're used. The Ternary Operator approach will not work since we will add a separate Button. Instead, we will wrap our Buttons in a single condition.
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue) // Using blue since it is not deleting anything
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            // By calling .toggle() we flip the Bool, update the View and save the change to permanent storage
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green) // Using green since it is a "good" Button
+                    }
+                }
+                // Helping SwiftUI to understand that this entire code is related to ONE prospect, add a Tag
+                .tag(prospect) // When I am selected, I should add to the set or remove from the set the prospect object
             }
             .navigationTitle(title)
             .toolbar {
-                Button("Scan", systemImage: "qrcode.viewfinder") {
-                    // Go ahead and show our Scanner
-                    isShowingScanner = true
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
+                }
+                
+                // Showing an Edit/Done Button to allow selecting and deleting multiple entries. We will trigger the mass deletion with a second Button, located at the bottom of the screen just like Apple does.
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
+                // This will be shown only when there are selections available to delete
+                if selectedProspects.isEmpty == false {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Delete Selected", action: delete)
+                    }
                 }
             }
             .sheet(isPresented: $isShowingScanner) {
-                // Creating a ScannerView takes at least 3 parameters:
-                // 1. An Array of the types of codes we want to scan (iOS supports a lot of them);
-                // 2. A String to use as simulated data (since we are in XCode and therefore we cannot use any real Camera), so CodeScanner can automatically present a replacement UI in this debug mode and return back this String;
-                // 3. A completion function to use.
                 CodeScannerView(codeTypes: [.qr],
                                 simulatedData: "Paul Hudson\npaul@hackingwithswift.com",
                                 completion: handleScan)
@@ -85,23 +113,27 @@ struct ProspectsView: View {
     }
     
     
-    func handleScan(result: Result<ScanResult, ScanError>) { // Result contains either a ScanResult or a ScanError
+    func handleScan(result: Result<ScanResult, ScanError>) {
         isShowingScanner = false
         
-        // The data we are passing back is a "name\nemail". So, if the scanning was successful, we can pull apart that code into those two components and create a new Prospect object from it. If the scanning fails, we will just put an error out.
         switch result {
-        case .success(let result): // If it was successful, give me the result inside that
+        case .success(let result):
             let details = result.string.components(separatedBy: "\n")
             
-            // We could scan a QR Code from a cereal box, for example. So, did we get exactly two pieces of information from that QR Code?
             guard details.count == 2 else { return }
             
-            // Yay! We got exactly 2 pieces of information
             let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
             modelContext.insert(person)
             
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func delete() {
+        // Looping over all selected prospects and deleting them
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
