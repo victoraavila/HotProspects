2c969d77ce52ab817940e460d3573740c55981a7
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-18 19:53:58 -0300

    Adding a context menu to an image

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -8,12 +8,17 @@
 import CoreImage.CIFilterBuiltins
 import SwiftUI
 
+// With a little more effort, we can make a functionality with which the user can share their QR Code outside the app.
+// For this, we will use the ShareLink View, placing it inside a .contextMenu() modifier.
+
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
+    
+    @State private var qrCode = UIImage()
     
     var body: some View {
         NavigationStack {
@@ -28,16 +33,48 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        // Previously, it would make our UIImage by calling generateQRCode() again
+                        
+                        // We can't share UIImages, only SwiftUI's Images. Therefore, let's convert it
+                        // This creates a Button in the Context Menu
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                        
+                        // We could save part of the work by creating the QR Code only one time instead of twice (Caching)
+                        // This is also problematic because we are passing in the same hardcoded String twice. In a situation where we have to change it, we would've got to remind to change it in both places
+                        // We will do caching by creating a new @State property to store the UIImage that was made
+                        
+                        // Also, we've got to add the Permission Request asking to save the qrCode UIImage among the phone images
+                        // Blue icon HotProspects > Targets > Info > Right click and choose Add Row > Privacy - Photo Library Additions Usage Description. Write "We want to save your QR code." in the Value column.
+                        
+                        // The code above causes a runtime issue: "Modifying state during view update, this will cause undefined behavior."
+                        // This happens because the way our View is recursive: making the View body changes the QR Code, which makes the View body again and changes the QR Code... (the image is an @State property).
+                        // The view is rendered, calling body.
+                        // body calls generateQRCode(from:).
+                        // generateQRCode(from:) modifies the qrCode @State property.
+                        // Modifying qrCode triggers a view update.
+                        // The view update causes body to be called again.
+                        // The cycle repeats from step 2.
+                        // To fix it, we have to make the Image View use our cached QR Code. However, we still have to call generateQRCode(from: "\(name)\n\(emailAddress)") somewhere, which will be along with .onAppear() and with .onChange(). This way, the QR Code will be updated when the View is first shown and when name or email address is changed. We will create an auxiliary method that will update our QR Code.
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: emailAddress, updateCode)
         }
     }
     
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+    }
+    
+    // This method now will implicitly make sure to update the @State property with the UIImage before sending it back
     func generateQRCode(from string: String) -> UIImage {
         filter.message = Data(string.utf8)
         
--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -32,11 +32,11 @@ struct ProspectsView: View {
     
     @State private var isShowingScanner = false
     
-    @State private var selectedProspects = Set<Prospect>() // By default, no selections.
+    @State private var selectedProspects = Set<Prospect>()
     
     var body: some View {
         NavigationStack {
-            List(prospects, selection: $selectedProspects) { prospect in // The selection is bound to selectedProspects
+            List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
@@ -45,32 +45,23 @@ struct ProspectsView: View {
                         .foregroundStyle(.secondary)
                 }
                 .swipeActions {
-                    // Swipe Actions don't play nicely with the .onDelete() modifier, so we've gotta make Delete work by hand:
-                    // 1. We will add an individual swipe to delete like .onDelete();
-                    // 2. We will also add a multiple selection option that let users remove multiple entries at the same time. This means adding an @State to store the active selection.
-                    // We have to add the Delete Button first in the List so it becomes automatically attached to the full swipe to activate functionality, which is the default behavior.
-                    Button("Delete", systemImage: "trash", role: .destructive) {// The destructive role is because this is a bad Button
+                    Button("Delete", systemImage: "trash", role: .destructive) {
                         modelContext.delete(prospect)
-                        // Remember that the original Swipe to Delete shows the word "Delete" when swipping. However, Apple tells us to use only icons when having multiple options to choose.
                     }
                     
-                    // We need a way to move people from the Uncontacted Tab to the Contacted Tab: we will add a Swipe Action to the VStack in ProspectView to do so.
-                    // Since ProspectsView is shared between 3 Tabs, we've got to make sure the Swipe Action is correct no matter where they're used. The Ternary Operator approach will not work since we will add a separate Button. Instead, we will wrap our Buttons in a single condition.
                     if prospect.isContacted {
                         Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
                             prospect.isContacted.toggle()
                         }
-                        .tint(.blue) // Using blue since it is not deleting anything
+                        .tint(.blue)
                     } else {
                         Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
-                            // By calling .toggle() we flip the Bool, update the View and save the change to permanent storage
                             prospect.isContacted.toggle()
                         }
-                        .tint(.green) // Using green since it is a "good" Button
+                        .tint(.green)
                     }
                 }
-                // Helping SwiftUI to understand that this entire code is related to ONE prospect, add a Tag
-                .tag(prospect) // When I am selected, I should add to the set or remove from the set the prospect object
+                .tag(prospect)
             }
             .navigationTitle(title)
             .toolbar {
@@ -80,12 +71,10 @@ struct ProspectsView: View {
                     }
                 }
                 
-                // Showing an Edit/Done Button to allow selecting and deleting multiple entries. We will trigger the mass deletion with a second Button, located at the bottom of the screen just like Apple does.
                 ToolbarItem(placement: .topBarLeading) {
                     EditButton()
                 }
                 
-                // This will be shown only when there are selections available to delete
                 if selectedProspects.isEmpty == false {
                     ToolbarItem(placement: .bottomBar) {
                         Button("Delete Selected", action: delete)
@@ -131,7 +120,6 @@ struct ProspectsView: View {
     }
     
     func delete() {
-        // Looping over all selected prospects and deleting them
         for prospect in selectedProspects {
             modelContext.delete(prospect)
         }
