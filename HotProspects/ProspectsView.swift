eff4efac8d3441141fb1dbff31423fdd4a2ff22c
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-19 00:28:50 -0300

    Posting notifications to the lock screen

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -9,6 +9,13 @@ import CodeScanner
 import SwiftData
 import SwiftUI
 
+// We will add another Button to our list of Swipe Actions letting users opt to be reminded to contact a particular person
+// This will use UserNotifications to make a new local notification and will be conditionally included in between the Swipe Actions (shown only if the user hasn't been contacted already).
+// To schedule this notification in the first time, we have to request for authorization to show notifications on the Lock Screen. We have to be careful to handle the situation in which the user has disabled the permission afterwards. For this, we could call requestNotification() every time we want to post a notification. In the first time, an alert will be shown. In all other times, it will immediately return success or failure based on the previous response.
+// However, we will act differently: we will request getNotificationSettings() and use that to determine whether to make a new notification or to request permission. The settings object gives us properties, such as an alert setting to check whether we can show an alert or not (if not, we can only display the red badge). If the new permission was successfully accepted, then we will also show a notification.
+// We will create a closure to schedule a notification and use it in all successful cases.
+import UserNotifications
+
 struct ProspectsView: View {
     enum FilterType {
         case none, contacted, uncontacted
@@ -59,6 +66,13 @@ struct ProspectsView: View {
                             prospect.isContacted.toggle()
                         }
                         .tint(.green)
+                        
+                        // Adding an extra Button for scheduling a notification to contact
+                        // This particular Button should only appear for prospects which are currently not contacted
+                        Button("Remind Me", systemImage: "bell") {
+                            addNotification(for: prospect)
+                        }
+                        .tint(.orange)
                     }
                 }
                 .tag(prospect)
@@ -122,6 +136,51 @@ struct ProspectsView: View {
     func delete() {
         for prospect in selectedProspects {
             modelContext.delete(prospect)
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        // A closure which we can call whenever we need in the future
+        // This closure will be called in all successful scenarios and is responsible for creating a notification for the current prospect
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            // Defining when to show the notification
+//            var dateComponents = DateComponents()
+//            dateComponents.hour = 9 // Showing it at 9 a.m.
+//            
+//            // We have to use UNCalendarNotificationTrigger since we are defining a datetime target
+//            // Don't repeat it every day, just once
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            
+            // Uncomment this for testing purposes, so it will trigger after 5 seconds elapsed
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            // The identifier is a random identifier
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        // Using getNotificationSettings() and requestAuthorization() to make sure we only schedule notification when we are allowed to
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                // Below, options is what we want to show on the screen
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    // If the authorization was just granted
+                    if success {
+                        addRequest()
+                    } else if let error {
+                        print(error.localizedDescription) // This will print "The user denied" or something like it
+                    }
+                }
+            }
         }
     }
 }
