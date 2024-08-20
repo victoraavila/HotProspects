bd8773277c5dc59d1d8aedcd5851e131c878734f
Víctor Ávila <victoraavila2000@gmail.com>
2024-08-20 16:26:29 -0300

    Solved Challenge 1: added an icon to show if the prospect was contacted in the Everyone Tab

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -44,12 +44,22 @@ struct ProspectsView: View {
     var body: some View {
         NavigationStack {
             List(prospects, selection: $selectedProspects) { prospect in
-                VStack(alignment: .leading) {
-                    Text(prospect.name)
-                        .font(.headline)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        
+                        Text(prospect.emailAddress)
+                            .foregroundStyle(.secondary)
+                    }
                     
-                    Text(prospect.emailAddress)
-                        .foregroundStyle(.secondary)
+                    Spacer()
+                    
+                    if filter == .none {
+                        Image(systemName: prospect.isContacted ? "person.crop.circle.fill.badge.checkmark" : "person.crop.circle.badge.xmark")
+                            .font(.title)
+                            .foregroundStyle(prospect.isContacted ? .green : .red)
+                    }
                 }
                 .swipeActions {
                     Button("Delete", systemImage: "trash", role: .destructive) {
